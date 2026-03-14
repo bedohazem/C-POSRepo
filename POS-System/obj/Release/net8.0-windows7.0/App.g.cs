50bc89bb32b5e431a7f621a213f379aa5ccaaac3
bedohazem <bedohazem80@gmail.com>
2026-03-14 11:29:45 +0200

    .

--- POS-System/obj/Release/net8.0-windows7.0/App.g.cs
+++ POS-System/obj/Release/net8.0-windows7.0/App.g.cs
@@ -47,7 +47,7 @@ namespace POS_System {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "10.0.3.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "10.0.4.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
@@ -67,7 +67,7 @@ namespace POS_System {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "10.0.3.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "10.0.4.0")]
         public static void Main() {
             POS_System.App app = new POS_System.App();
             app.InitializeComponent();
